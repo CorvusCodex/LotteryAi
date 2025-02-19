fcf425066c54e5b9b0ace69d8e3ed53c9f7d6307
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2025-02-19 11:52:15 +0100

    Update LotteryAi.py

--- LotteryAi.py
+++ LotteryAi.py
@@ -15,7 +15,7 @@ def print_intro():
     print("Lottery prediction artificial intelligence")
     print("Created by: CorvusCodex")
     print("Github: https://github.com/CorvusCodex/")
-    print("Licence : MIT License")
+    print("Licence: MIT License")
     print("Support my work:")
     print("BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3")
     print("ETH/BNB/POL: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0")
