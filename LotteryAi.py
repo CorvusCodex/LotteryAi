42b6fe74fb18f9afe0907f201c4ec77ff3aca97d
Corvus AI <56726555+CorvusCodex@users.noreply.github.com>
2025-07-11 08:49:59 +0200

    Update LotteryAi.py

--- LotteryAi.py
+++ LotteryAi.py
@@ -26,7 +26,6 @@ def print_intro():
         print("Support my work:")
         print("BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3")
         print("ETH/BNB/POL: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0")
-        # Print Solana address
         print("SOL: FsX3CsTFkRjzne2KiD8gjw3PEW2bYqezKfydAP55BVj7")
         # Print Buy Me a Coffee link
         print("Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex")
